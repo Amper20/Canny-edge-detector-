d603357bad04fee45905f326ff5ffe62f568a260
Dan BUTMALAI (98750) <dan.butmalai@stud.acs.upb.ro>
2020-11-17 11:14:52 +0200

    Added frame samples, and bmp converter

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Canny-edge-detector-
-
+To convert to bmp
+```
+sudo apt-get install graphicsmagick-imagemagick-compat
+mogrify -compress None -format bmp frames/*.jpg
+```
 To compile: ```gcc -o ced_serial ced_serial.c -lm```
--- Video2Frame.py
+++ Video2Frame.py
@@ -6,7 +6,7 @@ idx = 0
 while success and idx <= 100:
     if(count % 50 == 0):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("frames/%d.bmp" % idx, gray)     # save frame as JPEG file      
+        cv2.imwrite("frames/%d.png" % idx, gray)     # save frame as JPEG file      
         idx += 1
     success,image = vidcap.read()
     count += 1
