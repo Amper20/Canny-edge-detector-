5be02e075cd4ada45d9248331f5c76bb39ff39be
Master <butmalai.dan@gmail.com>
2020-11-24 12:01:54 +0200

    images

--- Video2Frame.py
+++ Video2Frame.py
@@ -3,10 +3,10 @@ vidcap = cv2.VideoCapture('videoplayback.webm')
 success,image = vidcap.read()
 count = 0
 idx = 0
-while success and idx <= 100:
+while success and idx <= 2:
     if(count % 50 == 0):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.resize(gray,(7680, 4320), interpolation = cv2.INTER_AREA)
+        gray = cv2.resize(gray,(int(15360*3/4), int(8640*3/4)), interpolation = cv2.INTER_AREA)
         cv2.imwrite("frames/%d.png" % idx, gray)     # save frame as JPEG file      
         idx += 1
     success,image = vidcap.read()
